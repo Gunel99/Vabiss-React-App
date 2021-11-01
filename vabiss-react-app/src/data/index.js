1090caa43c9f32b75d264020e954746f4ac22057
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2021-11-01 21:24:00 +0400

    Added data to table

---
+++ vabiss-react-app/src/data/index.js
@@ -0,0 +1,21 @@
+export const data = [
+  { id: 1, name: "Bakı", status: true },
+  { id: 2, name: "Şamaxı", status: false },
+  { id: 3, name: "Neftçala", status: false },
+  { id: 4, name: "Maştağa", parent_id: "1", status: true },
+  { id: 5, name: "Buzovna", parent_id: "1", status: false },
+  { id: 6, name: "Şüvəlan", parent_id: "1", status: false },
+  { id: 7, name: "Balaxanı", parent_id: "1", status: true },
+  { id: 8, name: "Zığ", parent_id: "1", status: true },
+  { id: 9, name: "Binə", parent_id: "1", status: true },
+  { id: 10, name: "Hacıqədirli", parent_id: "2", status: false },
+  { id: 11, name: "Mirikənd", parent_id: "2", status: false },
+  { id: 12, name: "Ovçulu", parent_id: "2", status: true },
+  { id: 13, name: "Sis", parent_id: "2", status: true },
+  { id: 14, name: "Boyat", parent_id: "3", status: false },
+  { id: 15, name: "Dördlər", parent_id: "3", status: false },
+  { id: 16, name: "Ərəbqardaşbəyli", parent_id: "3", status: true },
+  { id: 17, name: "Xanməmmədli", parent_id: "3", status: false },
+  { id: 18, name: "Kürkənd", parent_id: "3", status: true },
+];
+
